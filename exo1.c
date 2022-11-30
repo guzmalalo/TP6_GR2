55bd4b16c72aa64e1a5e43e96feb7c9b7b6cb8c6
Eduardo Guzman <eduardo.guzman@innovamics.com>
2022-11-30 11:24:20 +0100

    Avancement exo 1

--- exo1.c
+++ exo1.c
@@ -1,6 +1,28 @@
-
+#include <stdio.h>
 #include "exo1.h"
 
+
+void initialize(int* pa, float* pb){
+
+    *pa = 0;
+    *pb = 0;
+
+}
+
 int main(){
+    int a;
+    float b;
+
+    printf("\nAvant \n");
+    printf(" a = %d", a);
+    printf(" b = %f", b);
+
+    initialize(&a, &b);
+
+    printf("\nApres \n");
+    printf(" a = %d", a);
+    printf(" b = %f", b);
+
+
     return 0;
 }
