57937e5474ffb2371e1835858f71854f924855f9
Eduardo Guzman <eduardo.guzman@innovamics.com>
2022-12-07 19:12:47 +0100

    Correction exo 2

--- exo2.c
+++ exo2.c
@@ -1,8 +1,39 @@
-
-
+#include <stdio.h>
 #include "exo2.h"
 
 int main(){
+    // déclaration des variables
+    int a = 5;
+    int b = 2;
+    int q = 0;
+    int r = 0;
+
+    // procedure
+    pQR(a,b,&q,&r);
+
+    printf("quotient 5/2 = %d \n", q);
+    printf("reste 5/2 = %d \n", r);
+
+    scanf("%d", &a);
+    // fonction
+    r = fQR(a,b,q);
+
+    printf("quotient 5/2 = %d \n", q);
+    printf("reste 5/2 = %d \n", r);
 
     return 0;
+}
+
+void pQR(const int a, const int b, int* q, int* r){
+    *q = a/b;
+    *r = a%b;
+
+}
+
+int fQR(int a, int b, int* q){
+    int r;
+    *q = a/b;
+    r = a%b;
+
+    return r;
 }
