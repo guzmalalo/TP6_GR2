05c93a9c4f5b6b61170ea627aa12bb0320ed9d5b
Eduardo Guzman <eduardo.guzman@innovamics.com>
2022-12-07 18:33:33 +0100

    Mise en place de l'exo 2.
    - Ajout cmakelist et main

---
+++ exo2.c
@@ -0,0 +1,8 @@
+
+
+#include "exo2.h"
+
+int main(){
+
+    return 0;
+}
---
+++ exo2.h
@@ -0,0 +1,8 @@
+//
+// Created by Eduardo Guzman Maldonado on 07/12/2022.
+//
+
+#ifndef TP6_EXO2_H
+#define TP6_EXO2_H
+
+#endif //TP6_EXO2_H
